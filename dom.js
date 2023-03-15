0e8cff6d7618070861ff89e59b5c3c6d175e9ab7
gautamraj873 <gautamraj873@gmail.com>
2023-03-15 23:42:47 +0530

    item5 added inQuerySelector && QuerySelectorALL to new branch

--- dom.js
+++ dom.js
@@ -1,7 +1,18 @@
-let items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green';
+let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none';
+
+var odd=document.querySelectorAll('li:nth-child(odd');
+for(let i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor = 'green';
+}
+
 
 for(let i=0;i<items.length;i++)
 {
     items[i].style.fontWeight = 'bold';
 }
+
