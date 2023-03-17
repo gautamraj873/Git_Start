a5cc998f02c37f87c4ee1ad141f08aa65dd57b6a
gautamraj873 <gautamraj873@gmail.com>
2023-03-17 12:18:20 +0530

    serialized & deserialized

--- locStorage.js
+++ locStorage.js
@@ -5,8 +5,24 @@ form.addEventListener('submit', addItem);
 function addItem(e){
     e.preventDefault();
 
-    let inputValue = document.getElementById('name').value;
+    let nameValue = document.getElementById('username').value;
+    let emailValue = document.getElementById('emailId').value;
+    let phoneValue = document.getElementById('phoneNo').value;
 
-    localStorage.setItem('Name', inputValue);
+    // localStorage.setItem('name', nameValue);
+    // localStorage.setItem('email', emailValue);
+    // localStorage.setItem('phoneNo', phoneValue);
+
+    const obj = {
+        username : nameValue,
+        emailId : emailValue,
+        phoneNo : phoneValue
+    };
+
+    let obj_serialized = JSON.stringify(obj);
+
+    localStorage.setItem('userDetails', obj_serialized);
+
+    let obj_deserialized = JSON.parse(localStorage.getItem('userDetails'));
+    console.log(obj_deserialized);
 };
-
