da77bfa19c4726a40df07f126e3c4482cbabc67c
gautamraj873 <gautamraj873@gmail.com>
2023-03-31 20:41:05 +0530

    axios linked to store the data

--- locStorage.js
+++ locStorage.js
@@ -17,53 +17,74 @@ function addItem(e){
         phoneNo : phoneValue
     };
 
-    let itemsArray = [];
-    if(localStorage.getItem('items')){
-        itemsArray = JSON.parse(localStorage.getItem('items'));
-    }
-    itemsArray.push(obj);
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-
-    showUserOnScreen();
-
-    form.reset();
+    axios.post('https://crudcrud.com/api/63b16f4112514649adb17463679e108b/users', obj)
+      .then(function (response) {
+        showUserOnScreen();
+        form.reset();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
 
 function showUserOnScreen(){
     parentElem.innerHTML = "";
-    let itemsArray = JSON.parse(localStorage.getItem('items'));
 
-    if(itemsArray){
-        for(let i=0; i<itemsArray.length; i++){
-            let li = document.createElement('li');
-            li.innerHTML = itemsArray[i].username + " - " + itemsArray[i].emailId + " - " + itemsArray[i].phoneNo + " ";
-            let deleteButton = document.createElement('button');
-            deleteButton.innerHTML = "Delete";
-            deleteButton.setAttribute('data-index', i);
-            let editButton = document.createElement('button');
-            editButton.innerHTML = "Edit";
-            editButton.setAttribute('data-index', i);
-            li.appendChild(deleteButton);
-            li.appendChild(editButton);
-            parentElem.appendChild(li);
+    axios.get('https://crudcrud.com/api/63b16f4112514649adb17463679e108b/users')
+      .then(function (response) {
+        let itemsArray = response.data;
+
+        if(itemsArray){
+            localStorage.setItem('items', JSON.stringify(itemsArray)); // Store data in local storage
+            for(let i=0; i<itemsArray.length; i++){
+                let li = document.createElement('li');
+                li.innerHTML = itemsArray[i].username + " - " + itemsArray[i].emailId + " - " + itemsArray[i].phoneNo + " ";
+                let deleteButton = document.createElement('button');
+                deleteButton.innerHTML = "Delete";
+                deleteButton.setAttribute('data-id', itemsArray[i]._id);
+                let editButton = document.createElement('button');
+                editButton.innerHTML = "Edit";
+                editButton.setAttribute('data-id', itemsArray[i]._id);
+                li.appendChild(deleteButton);
+                li.appendChild(editButton);
+                parentElem.appendChild(li);
+            }
         }
-    }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
 
 function deleteOrUpdateItem(e){
     if(e.target.tagName === "BUTTON"){
-        let index = e.target.getAttribute('data-index');
-        let itemsArray = JSON.parse(localStorage.getItem('items'));
+        let id = e.target.getAttribute('data-id');
+
         if(e.target.innerHTML === "Delete") {
-            itemsArray.splice(index, 1);
-            localStorage.setItem('items', JSON.stringify(itemsArray));
+            axios.delete(`https://crudcrud.com/api/63b16f4112514649adb17463679e108b/users/${id}`)
+              .then(function (response) {
+                showUserOnScreen();
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
         } else if(e.target.innerHTML === "Edit") {
-            let user = itemsArray[index];
-            document.getElementById('username').value = user.username;
-            document.getElementById('emailId').value = user.emailId;
-            document.getElementById('phoneNo').value = user.phoneNo;
-            itemsArray.splice(index, 1);
-            localStorage.setItem('items', JSON.stringify(itemsArray));
+            let itemsArray = JSON.parse(localStorage.getItem('items')); // Retrieve data from local storage
+            let index = -1;
+            for(let i=0; i<itemsArray.length; i++) {
+              if(itemsArray[i]._id === id) {
+                index = i;
+                break;
+              }
+            }
+            if(index !== -1) {
+              let user = itemsArray[index];
+              document.getElementById('username').value = user.username;
+              document.getElementById('emailId').value = user.emailId;
+              document.getElementById('phoneNo').value = user.phoneNo;
+              itemsArray.splice(index, 1);
+              localStorage.setItem('items', JSON.stringify(itemsArray)); // Update data in local storage
+            }
         }
         showUserOnScreen();
     }
